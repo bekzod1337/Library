6f8b91735f4bd13d4c9f744fd467b03751c31a22
Bekzod <yahikooo7586@gmail.com>
2024-04-05 19:36:21 +0500

    yangi_ozgarish

--- main.py
+++ main.py
@@ -12,8 +12,14 @@ print("6. Kitob qo'shish")
 print("7. Chiqish")
 InputNumber = int(input("Quyidagilardan birini (raqamini) kiriting : "))
 if InputNumber == 1:
-    with open('kitoblar.txt', 'r') as kitoblar_royxati:
+    with open('kitoblar.txt', 'r+') as kitoblar_royxati:
+        kitoblar_royxati.seek(0)
         for i in kitoblar_royxati:
-            print(i.strip("\n"))
+            print(i.strip().split())
+        a = input()
+        
+        if a in kitoblar_royxati.read():
+            print("Success")
+        
 elif InputNumber == 2:
     pass
